d75b43805218e92c2d9ee07f8b18b11d2d77272c
Konstantinos Demartinos <konstantinos.demartinos@iota.org>
2026-02-19 13:19:34 +0200

    refactor(indexer): chunk snapshots dynamically

--- crates/iota-indexer/src/store/pg_indexer_store.rs
+++ crates/iota-indexer/src/store/pg_indexer_store.rs
@@ -1895,7 +1895,10 @@ impl IndexerStore for PgIndexerStore {
             )
             .collect::<Result<Vec<_>, _>>()?;
         let len = objects_snapshot.len();
-        let chunks = chunk!(objects_snapshot, self.config.parallel_objects_chunk_size);
+        let snapshot_parallel_writes = (self.blocking_cp.max_size() * 2) as usize;
+        let chunk_size =
+            (len / snapshot_parallel_writes).clamp(self.config.parallel_objects_chunk_size, 10_000);
+        let chunks = chunk!(objects_snapshot, chunk_size);
         // Throttle the parallel write to avoid exceeding the max-number of tokio
         // blocking threads
         futures::stream::iter(chunks)
@@ -1904,7 +1907,7 @@ impl IndexerStore for PgIndexerStore {
                 self.spawn_blocking_task(move |this| this.backfill_objects_snapshot_chunk(c))
                     .await
             })
-            .buffer_unordered(100)
+            .buffer_unordered(snapshot_parallel_writes)
             .try_collect::<Vec<_>>()
             .await
             .map_err(|e| {
@@ -2350,10 +2353,7 @@ impl IndexerStore for PgIndexerStore {
             })?;
 
         let elapsed = guard.stop_and_record();
-        info!(
-            elapsed,
-            "Persisted objects with {mutation_len} mutations and {deletion_len} deletions",
-        );
+        info!(elapsed, "Persisted {} objects", mutation_len + deletion_len);
         Ok(())
     }
 
