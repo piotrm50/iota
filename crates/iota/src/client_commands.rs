9f4a8499b0b951b64100561eec7b7d63f6d073cf
Thibault Martinez <thibault@iota.org>
2026-05-12 17:36:03 +0200

    remove comment

--- crates/iota/src/client_commands.rs
+++ crates/iota/src/client_commands.rs
@@ -1884,8 +1884,6 @@ impl IotaClientCommands {
                     })?)?;
                 let intent_msg = IntentMessage::new(intent, msg.clone());
                 let raw_intent_msg: String = Base64::encode(bcs::to_bytes(&intent_msg)?);
-                // let mut hasher = DefaultHash::default();
-                // hasher.update(bcs::to_bytes(&intent_msg)?);
                 let digest = intent_msg.signing_message();
 
                 let iota_signature = if auth_call_args.is_some() || auth_type_args.is_some() {
